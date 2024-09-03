ad5c6ca6271a3a95d1498f0dc3c5aac2eccfbbfd
codechini <76209471+codechini@users.noreply.github.com>
2024-09-04 01:12:48 +0530

    Add files via upload

---
+++ base/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="responsive vertical padding center-align middle-align">
+        <div>
+          <small className="">Blog template build with React by <a target="_blank" href="https://codechini.netlify.app/">@chinmay</a></small>
+        </div>
+        <div>
+          <a href="/">Back to top</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer
